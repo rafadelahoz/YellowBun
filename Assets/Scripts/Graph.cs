147904bcdbd2f92138bae0f0148e87c5e30e894c
Strauzen Ethermark <strauzen@users.noreply.github.com>
2017-11-18 18:50:07 +0100

    Created a node manager that can grab all the nodes in itself and create automatic connections amongst nodes according to the distance.

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour {
+
+    public Node[] graph;
+    public int maximumDistance = 8;
+
+	// Use this for initialization
+	void Start () {
+        graph = this.GetComponentsInChildren<Node>();
+
+		foreach (Node nodeA in graph)
+        {
+            Vector3 position = nodeA.GetComponent<Transform>().position;
+            foreach (Node nodeB in graph)
+            {
+                if (!nodeA.transform.position.Equals(nodeB.transform.position) && IsCloseEnough(nodeA, nodeB) && !nodeA.connectedWaypoints.Contains(nodeB))
+                {
+                    nodeA.connectedWaypoints.Add(nodeB);
+                }
+            }
+        }
+	}
+	
+    bool IsCloseEnough(Node nodeA, Node nodeB)
+    {
+        Vector3 positionA = nodeA.transform.position;
+        Vector3 positionB = nodeB.transform.position;
+
+        double distance = Vector2.Distance(new Vector2(positionA.x, positionA.y), new Vector2(positionB.x, positionB.y));
+        return distance <= maximumDistance;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    public List<Node> connectedWaypoints;
+    bool hasBeenChecked = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!hasBeenChecked)
+        {
+            Debug.Log("My list of nodes is: " + connectedWaypoints.Count);
+            hasBeenChecked = true;
+        }
+	}
+}
